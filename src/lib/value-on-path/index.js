bf3e3e569068113356f98ba12dcc00e7a7ca5c99
Macaully James Muir <dev@macaullyjamesmuir.com>
2015-07-07 16:03:14 +0200

    value-on-path now exports a function
    Since it's really a one-function module, it makes sense that
    module.exports should be a function instead of an object containing a
    function (otherwise we'd have to write
    `require("value-on-path").valueOnPath`, which isn't very pretty). This
    fixes the twitter API demo.

--- src/lib/value-on-path/index.js
+++ src/lib/value-on-path/index.js
@@ -1,11 +1,8 @@
 var _ = require("underscore");
 var angular = require("angular");
 
-module.exports = {
-    valueOnPath: function(object,path,removeRoot) {
-
-      return _.reduce((removeRoot ? path.substring(path.indexOf(".")+1) : path).split("."),function(memo,id){
-        return angular.isDefined(memo) ? memo[id] : null;
-      },object)
-    }
+module.exports = function(object,path,removeRoot) {
+  return _.reduce((removeRoot ? path.substring(path.indexOf(".")+1) : path).split("."),function(memo,id){
+    return angular.isDefined(memo) ? memo[id] : null;
+  },object);
 };
