1cb81101252852964ce3f4f8fc1393e336e0f575
Filip Kis <filip.kis@gmail.com>
2016-01-08 17:29:04 +0100

    Adding codepen tutorial config js

---
+++ endev-codepen-tutorial.js
@@ -0,0 +1,37 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+var createGuid = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+var guid;
+
+if (!getCookie('endevTutorial')) {
+    guid = createGuid();
+    setCookie('endevTutorial',guid);
+} else {
+    guid = getCookie('endevTutorial');
+}
+
+endev.firebaseProvider = {
+    path: "https://burning-fire-1948.firebaseio.com/" + guid
+}
