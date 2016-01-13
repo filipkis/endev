f62319ac4fb714e4f7f0853a7459188a78d7ebe6
Filip Kis <filip.kis@gmail.com>
2016-01-13 02:45:00 +0100

    Updates

--- endev.full.js
+++ endev.full.js
@@ -31097,6 +31097,8 @@ endevModule.directive("explain",function(){
 
 endevModule.directive("describe",['$endevProvider',function($endevProvider){
   return {
+    priority: 1001,
+    terminal: true,
     link: function(scope,element,attrs) {
       var yql = $endevProvider.getContext("yql").provider;
       if(!_.isUndefined(attrs.describe)) {
@@ -31104,7 +31106,11 @@ endevModule.directive("describe",['$endevProvider',function($endevProvider){
           var res = {
             parameters: _.map(desc.request.select.key,function(value){return _.pick(value,"name","type","required")})
           }
-          element[0].innerHTML = "<pre class='_endev_json_'>" + syntaxHighlight(JSON.stringify(res, undefined, 2)) + "</pre>";
+          var text = desc.name
+          if(desc.meta.documentationURL) {
+            text = text + " <a href='" + desc.meta.documentationURL + "'>documentation</a>"
+          }
+          element[0].innerHTML = text + "<pre class='_endev_json_'>" + syntaxHighlight(JSON.stringify(res, undefined, 2)) + "</pre>";
         });
       }
     }
